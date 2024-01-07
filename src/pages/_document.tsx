2205fb8ef007005880ccc583a0c096c1d54c4030
SANJAY-KUMAR-7 <sanjaykumarjat351728@gmail.com>
2024-01-07 22:49:23 +0530

    Added Favicon and vertical scroll to Evet details modal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link rel="icon" type="favicon" href="https://i.postimg.cc/J4pxPVkX/Jecrc-logo-gradient.png"/>
+        </Head>
       <body className='absolute md:relative' >
         <Main />
         <NextScript />
