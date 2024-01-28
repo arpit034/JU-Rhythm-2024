66f1eb28a54cd9e9f0816d11d8a814f4b4108341
arpit034 <arpitsha2003@gmail.com>
2024-01-28 13:05:50 +0530

    media new

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -12,17 +12,17 @@ export default function Technical() {
       "image": 'https://i.postimg.cc/VLCjGs2f/Pixellate.png',
       "aboutEvent": "Designing is a challenge, it requires a pioneering vision strengthened by a curiosity out of the ordinary; yet typical of any man. To design is to invent, innovate, and surprise. All this through a single tool   Pixelate is a contest in which participants are asked to design graphics using the given set of tock images and other resources. The event is divided into two rounds. The first is an   elimination round in which a short design problem will be given.",
       "eventDetail": [
-        "Registration Fee: Rs 300  || Team Composition: 1"
+        "Registration Fee: Rs 300  per person || Team Composition: 1"
       ],
       "rules": [
         	"Skill",
-"	Message/ Thought behind the design", 
-	"Only Canva is allowed to make posters.", 
-"	It should be made from scratch, no Templates should be used.",
-	"Eye catching",
-  "The decisions made by the judges would be final and binding.",
-	"Interaction with the viewer.",
-	"Creativity", 
+      "	Message/ Thought behind the design", 
+	   "Only Canva is allowed to make posters.", 
+     "	It should be made from scratch, no Templates should be used.",
+   	"Eye catching",
+    "The decisions made by the judges would be final and binding.",
+	   "Interaction with the viewer.",
+	   "Creativity", 
 
 
 
@@ -40,12 +40,12 @@ export default function Technical() {
       "alias": "Quick-Flick",
       "image": 'https://i.postimg.cc/tC2Prrz5/QUICK-FLICK.png',
       "aboutEvent": "Anything that can be written or thought of can be filmed. They say a picture is worth a thousand words then a film is worth a thousand times more. If you are an author whose novels are films, this is right place to publish.",
-      "eventDetail": ["Registration fee: Rs 300  || Team Composition: 1"],
+      "eventDetail": ["Registration fee: Rs 300 per person|| Team Composition: 1"],
       "rules": [
-        "A reel must be made which should cover the whole Rhythm 2024 including all the three days.",
-        "The clips must be shooted meanwhile the Rhythm is going on.",
+        "	A reel must be made which should cover all the events happening on the day.",
+        "	The clips must be recorded while the rhythm is going on.",
         "The reel should cast off with a trending sound.",
-        "The reel has to be uploaded on Instagram collaborated with ID:",
+        "The reel has to be uploaded on Instagram collaborated with ID: giving on event day.",
         "JUDGING CRITERIA",
         "Number of likes on the reel",
         "Number of views on the reel",
@@ -70,10 +70,10 @@ export default function Technical() {
       ],
       "rules": [
         "Individual Participation",
-        "Participants should have their own digital camera or DSLR’s.",
+        	"Format should be .jpg format not in raw format. ",
         "5 tasks will be provided on both the days which are supposed to be completed accordingly.",
         "The photos should be uploaded in the given link on time.",
-        "JUDGING CRITERIA",
+        "	No Editing should be done. ",
         "Amount of tasks completed on time",
         "Framework of the photos will be in concern.",
         "The photos captured in the phone and the camera will be evaluated separately via well-defined comparing manner."
@@ -96,11 +96,14 @@ export default function Technical() {
         "Registration Fee: Rs 2000 per team  || Team composition:4"
       ],
       "rules": [
-        "A short film should be made with a duration of 5-10 mins max.",
-        "The topic for the short film will be provided on the spot.",
-        "The final submission of the short film must inculcate a well-defined script along with the screenplay of the movie.",
+        "A short film should be made with a duration of 4-7 mins max.",
+        "	A film should have a glimpse of all the events happening on the day of shoot. ",
+        "	No restrictions on the usage of phones and DSLR.",
+        	"No clips should be there via unfair practices",
+      
+      
         "JUDGING CRITERIA",
-        "Story telling",
+        
         "Quality of video (1080p min_4k max) and sound.",
         "Script of the movie",
         "Video editing and sound scoring"
@@ -120,9 +123,10 @@ export default function Technical() {
       "aboutEvent": "",
       "eventDetail": ["Registration Fee: Rs600 per person (for two days) || Team Composition:1"],
       "rules": [
-        "Vlog duration should be about 5-10 mins max.",
-        "Day 1 and Day 2 should be covered during this shoot.",
-        "JUDGING CRITERIA",
+        "Vlog duration should be about 4-7 mins max.",
+        "Day 1 all the events should be covered during this shoot.",
+        	"Vlog should be uploaded on Day 1.",
+          "	Day 2  will be allotted for promotion.",
         "Video editing",
         "Sound and video quality (1080p min-4k max)",
         "Interaction with viewer",
